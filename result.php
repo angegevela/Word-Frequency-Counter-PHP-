17fd4e56b7ca40622e7342a1dd0edf3a1174451d
angegevela <202280079@psu.palawan.edu.ph>
2025-02-17 23:30:05 +0800

    Adding a html table code for displaying result after processed.

--- result.php
+++ result.php
@@ -1,3 +1,31 @@
 <?php 
 session_start();
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Word Frequency Results</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <h1>Word Frequency Results</h1>
+    
+    <?php
+    if (isset($_SESSION['wordFrequency'])) {
+        echo "<table border='1'><tr><th>Word</th><th>Frequency</th></tr>";
+        foreach ($_SESSION['wordFrequency'] as $word => $count) {
+            echo "<tr><td>" . htmlspecialchars($word) . "</td><td>" . $count . "</td></tr>";
+        }
+        echo "</table>";
+
+        // Clear session data after displaying
+        unset($_SESSION['wordFrequency']);
+    } else {
+        echo "<p>No results available.</p>";
+    }
+    ?>
+
+    <br><a href="index.php">Go Back</a>
